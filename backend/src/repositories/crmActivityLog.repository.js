7c89be61872a951be0885f0b569b1d888d6e3188
nh0kanh10 <buitringuyen1999@gmail.com>
2026-07-14 14:27:43 +0700

    update

--- backend/src/repositories/crmActivityLog.repository.js
+++
@@ -1,6 +0,0 @@
-import { findAll, findById, findOne, insertOne, updateById, deleteById } from "../db/index.js"
-
-const COL = "crmActivityLogs"
-
-export function getAll() { return findAll(COL) }
-export function create(data) { return insertOne(COL, data) }
